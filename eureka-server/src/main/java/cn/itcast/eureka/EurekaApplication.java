c3555f5dab09670d2468338e0b6c4c4648bcda91
zhengwenming <740747055@qq.com>
2024-03-27 18:19:07 +0800

    init proj
    spring cloud demo

---
+++ eureka-server/src/main/java/cn/itcast/eureka/EurekaApplication.java
@@ -0,0 +1,9 @@
+package cn.itcast.eureka;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+@SpringBootApplication
+@EnableE
+public class EurekaApplication {
+}
